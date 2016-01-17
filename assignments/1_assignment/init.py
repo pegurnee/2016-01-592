d4f9982244258b3f32bb715d6338529049c793d7
Eddie Gurnee <pegurnee@gmail.com>
2016-01-17 09:35:27 -0500

    updated init script

--- assignments/1_assignment/init.py
+++ assignments/1_assignment/init.py
@@ -29,7 +29,24 @@ for relation in relations:
       %s
     );
     ''' % (relation, ','.join(headers))
-
+    c.execute(sql)
     #c.execute('create table if not exists %s ( %s, primary key (%s) on conflict ignore);' % (relation, ','.join(headers), headers[0]))
 
-    c.executemany('insert into %s values (%s)' % (relation, ','.join('?' * len(headers))), read)
+    #c.executemany('insert into %s values (%s)' % (relation, ','.join('?' * len(headers))), read)
+    moresql = 'insert into %s values ' % relation
+    for i,line in enumerate(read):
+      #moresql += '(%s),' % (line)
+      lineish = '("' + '","'.join(line) + '"),'
+      moresql += lineish
+      if relation == 'enrollment':
+        pass
+        #print(lineish)
+      if i % 250 == 0:
+        c.execute(moresql[:-1])
+        moresql = 'insert into %s values ' % relation
+
+    c.execute(moresql[:-1])
+    moresql = 'insert into %s values ' % relation
+    conn.commit()
+     #('(' + '),('.join(read) + ')'))
+    #print(moresql)
