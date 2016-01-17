b41ed9da64a65729d34c6795f7818262a097ef29
Eddie Gurnee <pegurnee@gmail.com>
2016-01-17 09:53:38 -0500

    pt1

---
+++ assignments/1_assignment/1.sql
@@ -0,0 +1,9 @@
+select
+  count(*) as `online courses`,
+  STVTERM_ACYR_CODE as `year offered`
+from term, classes, enrollment
+where
+  STVSCHD_CODE='DEO'
+  and STVSCHD_CODE=SCHD_CODE_MEET1
+  and STVTERM_CODE=TERM_CODE_KEY
+group by STVTERM_ACYR_CODE;
