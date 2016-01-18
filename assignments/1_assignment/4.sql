e23ffeb73d40e1b3ece42bea99efc8a76468d14b
Eddie Gurnee <pegurnee@gmail.com>
2016-01-18 10:18:40 -0500

    pt4

---
+++ assignments/1_assignment/4.sql
@@ -0,0 +1,12 @@
+select
+  sum(cast(CREDIT_HOURS as Integer)) as `hour production`,
+  STVDEPT_DESC
+from
+  enrollment
+, department
+where
+  COLL_CODE = 'AS'
+  and TERM_CODE_KEY = '201620'
+  and STVDEPT_CODE = DEPT_CODE
+group by STVDEPT_DESC
+order by `hour production` desc;
