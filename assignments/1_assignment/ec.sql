6fa752a296fdca29cf72533dbf2f76f17cd97789
Eddie Gurnee <pegurnee@gmail.com>
2016-01-19 11:27:27 -0500

    hm.....

--- assignments/1_assignment/ec.sql
+++ assignments/1_assignment/ec.sql
@@ -1,12 +1,13 @@
 select
   DAYS as `Day`
-, (WITH RECURSIVE
-  get_time(x) AS (
+, (
+  WITH RECURSIVE
+    get_time(x) AS (
      SELECT 0
      UNION ALL
      SELECT x + 5 FROM get_time
       LIMIT 2355 / 5
-) select x from get_time) as `Time`
+    ) select x from get_time) as `Time`
 , sum(ACTUAL_ENROLLMENT) as `Number Enrolled`
 from
   enrollment
@@ -15,5 +16,5 @@ where
   and TERM_CODE_KEY = '201510'
   and cast(BEGIN_TIME1 as Integer) <= `Time`
   and cast(END_TIME1 as Integer) >= `Time`
-group by `Time`, `Day`
+/*group by `Time`, `Day`*/
 limit 10;
