8ae2b4fe16b830f6eedc47a981d59872d6c29278
Eddie Gurnee <pegurnee@gmail.com>
2016-01-19 14:28:11 -0500

    works. allowing for nonvalid dates

--- assignments/1_assignment/ec.sql
+++ assignments/1_assignment/ec.sql
@@ -1,20 +1,21 @@
+WITH RECURSIVE times as (
+  select 0 as the_time
+  union all
+  select the_time + 5
+  from times
+  where the_time < 2355
+)
 select
   DAYS as `Day`
-, (
-  WITH RECURSIVE
-    get_time(x) AS (
-     SELECT 0
-     UNION ALL
-     SELECT x + 5 FROM get_time
-      LIMIT 2355 / 5
-    ) select x from get_time) as `Time`
+, the_time as `Time`
 , sum(ACTUAL_ENROLLMENT) as `Number Enrolled`
 from
   enrollment
+, times
 where
   DAYS like '%T%'
   and TERM_CODE_KEY = '201510'
   and cast(BEGIN_TIME1 as Integer) <= `Time`
   and cast(END_TIME1 as Integer) >= `Time`
-/*group by `Time`, `Day`*/
-limit 10;
+group by
+  `Time`;
