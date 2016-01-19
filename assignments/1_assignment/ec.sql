8ff823e2145cc04a35b03dbae1b33ca67524b2ee
Eddie Gurnee <pegurnee@gmail.com>
2016-01-19 17:59:48 -0500

    extra credit complete

--- assignments/1_assignment/ec.sql
+++ assignments/1_assignment/ec.sql
@@ -4,36 +4,42 @@ select
 , `Number Enrolled`
 from
   (WITH RECURSIVE times as (
-    select '00:00' as the_time
+    select '23:55' as the_time
     union all
     select time(the_time, '+5 minutes')
     from times
     where the_time < time('23:55')
   )
   select
-    DAYS as `Day`
-  , time(
-      substr('00' || (BEGIN_TIME1 / 100), -2, 2)  || ':'
-        || substr('00' || (BEGIN_TIME1 % 100), -2, 2)
-    ) as start_time
-  , time(
-      substr('00' || (END_TIME1 / 100), -2, 2)    || ':'
-        || substr('00' || (END_TIME1 % 100), -2, 2)
-    ) as end_time
+    `Day`
   , the_time as `Time`
-  , sum(ACTUAL_ENROLLMENT) as `Number Enrolled`
+  , sum(`enrolled`) as `Number Enrolled`
   from
-    enrollment
+    (
+      select
+        'T' as 'Day'
+      , time(
+          substr('00' || (BEGIN_TIME1 / 100), -2, 2)  || ':'
+            || substr('00' || (BEGIN_TIME1 % 100), -2, 2)
+        ) as start_time
+      , time(
+          substr('00' || (END_TIME1 / 100), -2, 2)    || ':'
+            || substr('00' || (END_TIME1 % 100), -2, 2)
+        ) as end_time
+      , ACTUAL_ENROLLMENT as 'enrolled'
+      from
+        enrollment
+      where
+        DAYS like '%T%'
+        and TERM_CODE_KEY = '201510'
+      group by
+        start_time, end_time
+    ) as dayz
   , times
   where
-    DAYS like '%T%'
-    and TERM_CODE_KEY = '201510'
-    and
-      (
-        (BEGIN_TIME1 < `Time` and END_TIME1 > `Time`)
-        or BEGIN_TIME1 == `Time`
-        or END_TIME1 == `Time`
-      )
+    (`Time` > start_time and `Time` < end_time)
+    or start_time == `Time`
+    or end_time == `Time`
   group by
     `Time`) as raw
 group by
