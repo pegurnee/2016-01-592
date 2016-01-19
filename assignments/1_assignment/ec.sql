50eb7662f71460b2f54c1c0453e992dee145b106
Eddie Gurnee <pegurnee@gmail.com>
2016-01-19 17:43:24 -0500

    start_time and end_time appearing

--- assignments/1_assignment/ec.sql
+++ assignments/1_assignment/ec.sql
@@ -1,21 +1,42 @@
-WITH RECURSIVE times as (
-  select 0 as the_time
-  union all
-  select the_time + 5
-  from times
-  where the_time < 2355
-)
 select
-  DAYS as `Day`
-, the_time as `Time`
-, sum(ACTUAL_ENROLLMENT) as `Number Enrolled`
+  `Day`
+, (min(`raw`.`Time`) || '-' || max(`raw`.`Time`)) as `Time Span`
+, `Number Enrolled`
 from
-  enrollment
-, times
-where
-  DAYS like '%T%'
-  and TERM_CODE_KEY = '201510'
-  and cast(BEGIN_TIME1 as Integer) <= `Time`
-  and cast(END_TIME1 as Integer) >= `Time`
+  (WITH RECURSIVE times as (
+    select '00:00' as the_time
+    union all
+    select time(the_time, '+5 minutes')
+    from times
+    where the_time < time('23:55')
+  )
+  select
+    DAYS as `Day`
+  , time(
+      substr('00' || (BEGIN_TIME1 / 100), -2, 2)  || ':'
+        || substr('00' || (BEGIN_TIME1 % 100), -2, 2)
+    ) as start_time
+  , time(
+      substr('00' || (END_TIME1 / 100), -2, 2)    || ':'
+        || substr('00' || (END_TIME1 % 100), -2, 2)
+    ) as end_time
+  , the_time as `Time`
+  , sum(ACTUAL_ENROLLMENT) as `Number Enrolled`
+  from
+    enrollment
+  , times
+  where
+    DAYS like '%T%'
+    and TERM_CODE_KEY = '201510'
+    and
+      (
+        (BEGIN_TIME1 < `Time` and END_TIME1 > `Time`)
+        or BEGIN_TIME1 == `Time`
+        or END_TIME1 == `Time`
+      )
+  group by
+    `Time`) as raw
 group by
-  `Time`;
+  `Number Enrolled`
+order by
+  `Time Span`;
