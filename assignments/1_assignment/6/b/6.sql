a4e28016c6c1c66813952d7c2fd0f78d77db03fc
Eddie Gurnee <pegurnee@gmail.com>
2016-01-21 05:39:04 -0500

    classes in PRAY-H 520

---
+++ assignments/1_assignment/6/b/6.sql
@@ -0,0 +1,14 @@
+select
+  case
+    when substr(TERM_CODE_KEY, -2) == '10' then
+      'fall'
+    else
+      'winter'
+    end                             as `semester`
+, substr(TERM_CODE_KEY, 0, 5)       as `year`
+, DEPT_CODE || ' ' || CRSE_NUMBER   as `course`
+from
+  enrollment
+where
+  BLDG_CODE1='PRAY-H'
+  and ROOM_CODE1='520';
