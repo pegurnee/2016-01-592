2a9b3d637aa89e17d5383367389b71592d68fd03
Eddie Gurnee <pegurnee@gmail.com>
2016-01-22 05:52:23 -0500

    show num of non cosc in 520

--- assignments/1_assignment/6/b/6.sql
+++ assignments/1_assignment/6/b/6.sql
@@ -1,5 +1,6 @@
 select
-  case
+  TERM_CODE_KEY
+, case
     when substr(TERM_CODE_KEY, -2) == '10' then
       'fall'
     else
@@ -7,10 +8,18 @@ select
     end                             as `semester`
 , substr(TERM_CODE_KEY, 0, 5)       as `year`
 , DEPT_CODE || ' ' || CRSE_NUMBER   as `course`
+, case
+    when DEPT_CODE == 'COSC' then
+      0
+    else
+      count(DEPT_CODE)
+    end                             as `non cosc`
 from
   enrollment
 where
   BLDG_CODE1='PRAY-H'
   and ROOM_CODE1='520'
 group by
+  DEPT_CODE, TERM_CODE_KEY
+order by
   TERM_CODE_KEY;
