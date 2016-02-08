4add389db3d4c8b8b5951da4e9acd395b72a2ccf
Eddie Gurnee <pegurnee@gmail.com>
2016-02-08 15:28:34 -0500

    downloading files

--- assignments/2_assignment/init.py
+++ assignments/2_assignment/init.py
@@ -4,9 +4,16 @@ import ftplib
 def _write(block, file_to_write):
   file_to_write.write(block)
 
-ftp = ftplib.FTP('ftp.ncdc.noaa.gov', 'anonymous', 'egurnee@emich.edu')
 
-ftp.cwd('/pub/data/noaa/')
+years = range(1901, 1926)
+
+ftp = ftplib.FTP('ftp.ncdc.noaa.gov', 'anonymous', 'egurnee@emich.edu')
+ftp.cwd('/pub/data/noaa/1901/')
+
+files = ftp.nlst()
+for fname in files:
+  with open(fname, 'wb') as ofile:
+    ftp.retrbinary('RETR ' + fname, ofile.write)
 
 # dest = '1901'
 # ofile = open(dest, 'wb')
@@ -14,7 +21,6 @@ ftp.cwd('/pub/data/noaa/')
 
 print("File List: ")
 
-files = ftp.nlst()
 
 for f in files:
   print(f)
