f2383f3981f9740f90b96141cdfcde3e6d479697
Eddie Gurnee <pegurnee@gmail.com>
2016-02-11 15:39:20 -0500

    making all the directories

--- assignments/2_assignment/hdfs.sh
+++ assignments/2_assignment/hdfs.sh
@@ -2,8 +2,10 @@
 
 IN_LOC="in"
 
+hdfs dfs -mkdir -p "assign2/$IN_LOC"
+
 for f in $IN_LOC/*; do
   [[ -e $f ]] || continue
   # echo $f
-  hdfs dfs -put "in/$f" "assign2/dataset/$f"
+  hdfs dfs -put "in/$f" "assign2/$f"
 done
