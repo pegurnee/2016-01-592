65959dbf9f859e8e30daa02e437d5bb27abd33e9
Eddie Gurnee <pegurnee@gmail.com>
2016-02-28 07:05:01 -0500

    remove punctuation book downloads

--- assignments/3_assignment/src/book_download.sh
+++ assignments/3_assignment/src/book_download.sh
@@ -8,15 +8,15 @@ fi
 
 if [ ! -f "in/notebooks_of_davinci.txt" ]
   then
-    curl -o "in/notebooks_of_davinci.txt" http://www.gutenberg.org/files/5000/5000-8.txt
+    curl http://www.gutenberg.org/files/5000/5000-8.txt | sed -e 's/[[:punct:]]/ /g' > "in/trim_notebooks_of_davinci.txt"
 fi
 
 if [ ! -f "in/ulysses.txt" ]
   then
-    curl -o "in/ulysses.txt" http://www.gutenberg.org/cache/epub/4300/pg4300.txt
+    curl http://www.gutenberg.org/cache/epub/4300/pg4300.txt | sed -e 's/[[:punct:]]/ /g' > "in/trim_ulysses.txt"
 fi
 
 if [ ! -f "in/outline_of_science.txt" ]
   then
-    curl -o "in/outline_of_science.txt" http://www.gutenberg.org/cache/epub/20417/pg20417.txt
+    curl http://www.gutenberg.org/cache/epub/20417/pg20417.txt | sed -e 's/[[:punct:]]/ /g' >  "in/trim_outline_of_science.txt"
 fi
