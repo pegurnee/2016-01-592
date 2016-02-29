c5516e8a6e30b5aca51586847afe7dbd668b27b7
Eddie Gurnee <pegurnee@gmail.com>
2016-02-29 07:19:53 -0500

    moved and altered runadoop

---
+++ assignments/3_assignment/run-doop-a.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+I_LOCATION=assign3/in
+O_LOCATION=assign3/out1
+
+S_LOCATION=src
+STREAM_JAR=/usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-mr1.jar
+
+hdfs dfs -rm -r $O_LOCATION
+
+hadoop jar $STREAM_JAR -file $S_LOCATION/mapper.py -mapper $S_LOCATION/mapper.py -file $S_LOCATION/reducer.py -reducer $S_LOCATION/reducer.py -input $I_LOCATION -output $O_LOCATION
--- assignments/3_assignment/src/run-doop-a.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-
-hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-mr1.jar -file mapper.py-mapper mapper.py -file reducer.py -reducer reducer.py -input assign3/in/* -output assign3/out1
