bc1b62959c92876245ed6830389f520ba64e3afe
Eddie Gurnee <pegurnee@gmail.com>
2016-03-02 08:22:16 -0500

    rename mapper and reducer to specific mappers and reducers

--- assignments/3_assignment/run-doop-a.sh
+++ assignments/3_assignment/run-doop-a.sh
@@ -9,8 +9,8 @@ STREAM_JAR=/usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-mr1
 hdfs dfs -rm -r $O_LOCATION
 
 hadoop jar $STREAM_JAR \
--file $S_LOCATION/mapper.py -mapper $S_LOCATION/mapper.py \
--file $S_LOCATION/reducer.py -reducer $S_LOCATION/reducer.py \
+-file $S_LOCATION/a-mapper.py -mapper $S_LOCATION/a-mapper.py \
+-file $S_LOCATION/a-reducer.py -reducer $S_LOCATION/a-reducer.py \
 -input $I_LOCATION/* -output $O_LOCATION
 
 bash src/gen_report.sh
