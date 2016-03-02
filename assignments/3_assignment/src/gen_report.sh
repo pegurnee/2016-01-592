2ef894e779fb8d456fd9719379e0b5255a235af6
Eddie Gurnee <pegurnee@gmail.com>
2016-03-02 08:13:12 -0500

    report looks prettier

--- assignments/3_assignment/src/gen_report.sh
+++ assignments/3_assignment/src/gen_report.sh
@@ -2,12 +2,13 @@
 
 O_LOCATION=assign3/out1
 REPORT="report.txt"
+DIVIDER="________________\n"
 
 hdfs dfs -get $O_LOCATION/p*
 
-echo "Head 10:" > $REPORT
-head p*         >> $REPORT
-echo "Tail 10:" >> $REPORT
-tail p*         >> $REPORT
+echo "$DIVIDER\nHead 10:" > $REPORT
+head p*                   >> $REPORT
+echo "$DIVIDER\nTail 10:" >> $REPORT
+tail p*                   >> $REPORT
 
 rm p*
