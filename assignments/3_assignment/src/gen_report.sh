57d26a7ab062af6934fb6d456ff091cfa5554ad9
Eddie Gurnee <pegurnee@gmail.com>
2016-03-06 08:39:58 -0500

    confusing our friends and enemies

--- assignments/3_assignment/src/gen_report.sh
+++ assignments/3_assignment/src/gen_report.sh
@@ -7,9 +7,9 @@ DIVIDER="________________\n"
 
 hdfs dfs -get $O_LOCATION/p*
 
-echo "$DIVIDER\nHead 10:" > $REPORT
-head p*                   >> $REPORT
-echo "$DIVIDER\nTail 10:" >> $REPORT
-tail p*                   >> $REPORT
+echo -e "$DIVIDER\nHead 10:"  > $REPORT
+head p*                       >> $REPORT
+echo -e "$DIVIDER\nTail 10:"  >> $REPORT
+tail p*                       >> $REPORT
 
 rm p*
