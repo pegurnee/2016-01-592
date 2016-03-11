216295d64c376900fafe6dfb03c71b00412372a7
Eddie Gurnee <pegurnee@gmail.com>
2016-03-11 09:32:46 -0500

    add final mapper

--- assignments/3_assignment/src/a-mapper.py
+++ assignments/3_assignment/src/a-mapper.py
@@ -15,4 +15,6 @@ for line in sys.stdin:
     # Reduce step, i.e. the input for reducer.py
     #
     # tab-delimited; the trivial word count is 1
-    print '%s\t%s' % (word, 1)
+    word = ''.join(ch for ch in word if ch not in '[]\\\'"`~!@#$%^&*()-_=+\{\}/?;:<>,.|')
+    if word:
+      print '%s\t%s' % (word, 1)
